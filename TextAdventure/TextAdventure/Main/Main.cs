17a3f0250b83189398c9acb65b05fb82a58d67eb
ravecode <ravejacob@gmail.com>
2015-10-24 20:24:18 +0100

    Began implementing key press logic + minor code refractor.

--- TextAdventure/TextAdventure/Main/Main.cs
+++ TextAdventure/TextAdventure/Main/Main.cs
@@ -1,48 +1,64 @@
 ﻿using System;
-using System.Reflection;
 
 namespace TextAdventure
 {
     class Main
     {
-        string TITLE = "Dungeon Crawler";
-        string VERSION = Assembly.GetExecutingAssembly().GetName().Version.ToString();
-        string CREATORS = "Jake and Codi";
+        bool isRunning = true;
 
-        bool IS_RUNNING = true;
-        ObsoleteRenderer renderer;
-
-        public void start()
+        public void Start()
         {
-            Console.Title = TITLE + " | Version " + VERSION + " | " + CREATORS;
-            
-            renderer = new ObsoleteRenderer();
-            renderer.ScreenInit(64,64);
-        }
-
-        public void loop()
-        {
-            while (IS_RUNNING)
+            while (isRunning)
             {
+                ObsoleteRenderer renderer = new ObsoleteRenderer();
+                renderer.ScreenInit(64, 64);
+
                 string[] temp = { " /\\ /\\ ",
                                   "(O _ O)" };
 
                 renderer.Clear();
 
-                    renderer.DrawSquare(0,0,64, 16, '~', ConsoleColor.Blue);
-                    renderer.DrawSprite(4, 4, temp, ConsoleColor.DarkMagenta);
-                    renderer.DrawSprite(1, 16, temp, ConsoleColor.Red);
-                    renderer.DrawSprite(7, 43, temp, ConsoleColor.Green);
-                    renderer.DrawSprite(30, 16, temp, ConsoleColor.DarkCyan);
-                    renderer.DrawSprite(2, 50, temp, ConsoleColor.Cyan);
-                    renderer.DrawSprite(20, 40, temp, ConsoleColor.Red);
-                    renderer.DrawSprite(50, 10, temp, ConsoleColor.Green);
-                    renderer.DrawString(10, 1, "HELLO WORLD", ObsoleteRenderer.textAlingment.middle);
+                renderer.DrawSquare(0, 0, 64, 16, '~', ConsoleColor.Blue);
+                renderer.DrawSprite(4, 4, temp, ConsoleColor.DarkMagenta);
+                renderer.DrawSprite(1, 16, temp, ConsoleColor.Red);
+                renderer.DrawSprite(7, 43, temp, ConsoleColor.Green);
+                renderer.DrawSprite(30, 16, temp, ConsoleColor.DarkCyan);
+                renderer.DrawSprite(2, 50, temp, ConsoleColor.Cyan);
+                renderer.DrawSprite(20, 40, temp, ConsoleColor.Red);
+                renderer.DrawSprite(50, 10, temp, ConsoleColor.Green);
+                renderer.DrawString(10, 1, "HELLO WORLD", ObsoleteRenderer.textAlingment.middle);
 
                 renderer.Render();
 
-                ConsoleKey tmp = Console.ReadKey().Key;
-                if (tmp == ConsoleKey.Escape) IS_RUNNING = false;
+                Input();
+            }
+        }
+
+        private void Input()
+        {
+            ConsoleKey keyPress = Console.ReadKey(true).Key;
+
+            switch (keyPress)
+            {
+                case ConsoleKey.Escape:
+                    isRunning = false;
+                    break;
+                case ConsoleKey.W:
+                case ConsoleKey.UpArrow:
+                    // go up
+                    break;
+                case ConsoleKey.S:
+                case ConsoleKey.DownArrow:
+                    // go down
+                    break;
+                case ConsoleKey.A:
+                case ConsoleKey.LeftArrow:
+                    // go left
+                    break;
+                case ConsoleKey.D:
+                case ConsoleKey.RightArrow:
+                    // go right
+                    break;
             }
         }
     }
