3f2476f84d1e6c77ef91f264907fcf34057b9a1f
ravecode <ravejacob@gmail.com>
2015-10-24 16:04:45 +0100

    Added Canvas.cs & changed Renderer.cs, git is high.

--- TextAdventure/TextAdventure/Renderer/Renderer.cs
+++ TextAdventure/TextAdventure/Renderer/Renderer.cs
@@ -8,5 +8,37 @@ namespace TextAdventure
 {
     class Renderer
     {
+        #region Singleton
+        // Create Renderer Singleton
+
+        static readonly Renderer instance = new Renderer();
+
+        static Renderer() { }
+        private Renderer() { }
+
+        public static Renderer Instance
+        {
+            get
+            {
+                return instance;
+            }
+        }
+
+        #endregion
+
+        public void Render()
+        {
+            // render all the things
+        }
+
+        public void DrawSprite(Sprite sprite, int posX, int posY, ConsoleColor color = ConsoleColor.White)
+        {
+            // draw all the sprites
+        }
+
+        public void DrawCanvas(Canvas canvas)
+        {
+            // draw all the canvases
+        }
     }
 }
