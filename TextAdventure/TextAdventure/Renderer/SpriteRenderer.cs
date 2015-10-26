0dfe4c18ff1972f044b6e6654ab1c11c71a7a4dd
EmblemCronius <errorcodebin_2@hotmail.com>
2015-10-26 19:06:28 +0100

    Partly Implemented basics of SpriteRenderer

--- TextAdventure/TextAdventure/Renderer/SpriteRenderer.cs
+++ TextAdventure/TextAdventure/Renderer/SpriteRenderer.cs
@@ -1,15 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace TextAdventure
 {
     class SpriteRenderer
     {
+
         // ===========================================
         // Will contain everything to render "Sprites"
         // ===========================================
+
+        #region ForPossibleDebug
+        //public int renderCount{ get { return spritesToRender.Count; } }
+        #endregion
+
+        private Queue<Sprite> spritesToRender = new Queue<Sprite>();
+
+        public void AddSpriteList(Sprite sprite)
+        {
+            spritesToRender.Enqueue(sprite);
+        }
+
+        public void ClearSpriteList()
+        {
+            spritesToRender.Clear();
+        }
+
+        public void RenderToCanvas(bool clearList)
+        {
+            foreach (Sprite item in spritesToRender)
+            {
+                // Render sprites here
+            }
+
+            if (clearList)
+            {
+                ClearSpriteList();
+            }
+        }
     }
 }
