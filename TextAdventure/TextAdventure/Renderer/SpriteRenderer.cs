b06bbdc6281e3d7336d00c1a3278eecaccf71569
EmblemCronius <errorcodebin_2@hotmail.com>
2015-10-30 10:26:09 +0100

    Partly implemented RenderToCanvas Method.

--- TextAdventure/TextAdventure/Renderer/SpriteRenderer.cs
+++ TextAdventure/TextAdventure/Renderer/SpriteRenderer.cs
@@ -12,10 +12,11 @@ namespace TextAdventure
 
         #region ForPossibleDebug
         //public int renderCount{ get { return spritesToRender.Count; } }
+        //public int renderCount{ get { return PreviousTotalDrawCycles } }
         #endregion
 
         private Queue<Sprite> spritesToRender = new Queue<Sprite>();
-
+        
         public void AddSpriteList(Sprite sprite)
         {
             spritesToRender.Enqueue(sprite);
@@ -26,11 +27,20 @@ namespace TextAdventure
             spritesToRender.Clear();
         }
 
-        public void RenderToCanvas(bool clearList)
+        public void RenderToCanvas(Canvas targetCanvas, bool clearList = false)
         {
             foreach (Sprite item in spritesToRender)
             {
-                // Render sprites here
+                int width = targetCanvas.frame.width;
+                int height = targetCanvas.frame.height;
+
+                for (int x = 0; x < width; x++)
+                {
+                    for (int y = 0; y < height; y++)
+                    {
+                        // Render sprite to canvas.
+                    }
+                }
             }
 
             if (clearList)
